28d8e91a3f294835523d619b39406002d1cbc7df
EduardoHdez <jhernandezg1403@gmail.com>
2026-01-14 18:22:16 -0600

    Corregir error de importacion List

--- Wilson/API.py
+++ Wilson/API.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import pandas as pd
+from typing import List
 
 app = FastAPI(title="Predictiones API")
 
